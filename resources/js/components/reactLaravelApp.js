0fec6c5839bf739241299611be5b73c6e5aa2fcf
Samuel Sanchez Flores <Samuel_SFlores@hotmail.com>
2023-03-09 14:04:46 -0600

    Start LaravelReact

---
+++ resources/js/components/reactLaravelApp.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import { Provider } from 'react-redux'
+// import { AppRoutes } from './routes/appRoutes';
+// import { store } from './store/store';
+
+export const ReactLaravelApp = () => {
+
+    return (
+        <div>
+            <h1>React Laravel App</h1>
+        </div>
+        // <Provider store={store}>
+        //   <AppRoutes />
+        // </Provider>
+    );
+}
+
+export default ReactLaravelApp;
